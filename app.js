a9533fdb27d1fa54229b7e276bd61c42f776f7e4
Nayan96 <nnayanamin96@gmail.com>
2021-12-13 09:41:29 +0530

    cors-updated

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ var usersRouter = require('./routes/users');
 const chapterRouter = require('./routes/chapter');
 const mongoose = require('mongoose');
 var app = express();
-app.use(cors());
-app.use(logger('dev'));
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.options('*', cors()) // include before other routes
+
 mongoose.connect(process.env.MONGO_URL,(err)=>{
     if(err)
         console.log(err);
